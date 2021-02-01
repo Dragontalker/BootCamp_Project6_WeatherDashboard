cbf2ef56c20d5a1f7d2dc7d6900d2fa07fb7e040
Richard Yang <richard.yang.tong@gmail.com>
2021-02-01 09:55:05 -0500

    Made changes to the api call to return calsius instead of fahrenheit.

--- Assets/JavaScripts/script.js
+++ Assets/JavaScripts/script.js
@@ -2,12 +2,12 @@
 const apiKey = "792ee81e691256c1aa741d76e3a33b8a";
 
 const testApi = async (city) => {
-    let test = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+    let test = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
     .then(response => response.json())
     .then(data => {
-        let nameValue = data['name'];
-        let tempValue = data['main']['temp'];
-        let descValue = data['weather'][0]['description'];
+        let nameValue = data.name;
+        let tempValue = data.main.temp;
+        let descValue = data.weather[0].description;
 
         console.log(`The name value from data is ${nameValue}`);
         console.log(`The temperature value from data is ${tempValue}`);
