9747ec949535bbdd338d3e39fc81ee995fbcf736
Richard Yang <richard.yang.tong@gmail.com>
2021-02-01 15:06:56 -0500

    Created a new function to fetch UV index data from new API.

--- Assets/JavaScripts/script.js
+++ Assets/JavaScripts/script.js
@@ -16,6 +16,16 @@ const fetchWeatherData = async (city) => {
     }); 
 }
 
+const fetchUVIndexData = async () => {
+    let latValue = localStorage.getItem("lonValue");
+    let lonValue = localStorage.getItem("latValue");
+    let apiCall = await fetch(`http://api.openweathermap.org/data/2.5/uvi?lat=${latValue}&lon=${lonValue}&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+        localStorage.setItem("UV", data.value);
+    })
+}
+
 console.log(`City: ${localStorage.getItem("cityName")}`);
 console.log(`Temperature: ${localStorage.getItem("temperature")}°F`);
 console.log(`Humidity: ${localStorage.getItem("humidity")}`);
@@ -24,6 +34,7 @@ console.log(`The wind speed is ${localStorage.getItem("windSpeed")}MPH`);
 console.log(`The longtitude value is ${localStorage.getItem("lonValue")}`);
 console.log(`The latitude value is ${localStorage.getItem("latValue")}`);
 console.log(`The image url for current date icon is ${localStorage.getItem("currentIconURL")}`);
+console.log(`The UV index valuue is ${localStorage.getItem("UV")}`);
 
 // document.getElementById('temprature').innerText = `Temperature: ${tempValue}°F`;
 // document.getElementById('humidity').innerText = `Humidity: ${humiValue}%`;
